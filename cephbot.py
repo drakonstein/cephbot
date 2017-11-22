69f1de77baf76f96b8e8fda9d14896cc747e368e
drakonstein <drakonstein+git@gmail.com>
2017-11-22 12:08:55 -0500

    Settings enabled for limitting user and channel interactions.

--- cephbot.py
+++ cephbot.py
@@ -10,18 +10,70 @@ import yaml
 # read config variables
 try:
     config = yaml.safe_load(open("config.yaml"))
-    CEPH_CLUSTER_ID = config['CEPH_CLUSTER_ID']
-    CEPH_CONF = config['CEPH_CONF']
-    CEPH_USER = config['CEPH_USER']
-    CEPH_KEYRING = config['CEPH_KEYRING']
+except:
+    print "config.yaml not found"
+    exit()
+try:
     SLACK_BOT_TOKEN = config['SLACK_BOT_TOKEN']
+except:
+    print "SLACK_BOT_TOKEN is not defined in config.yaml. This is needed to open a connection to the Slack API."
+    exit()
+try:
     SLACK_BOT_ID = config['SLACK_BOT_ID']
-    HELP_MSG = config['HELP_MSG']
+except:
+    print "SLACK_BOT_ID is not defined in config.yaml. This is needed to know when cephbot should listen."
+    exit()
+try:
+    SLACK_USER_IDS = config['SLACK_USER_IDS']
+except:
+    SLACK_USER_IDS = None
+if not SLACK_USER_IDS:
+    print "Any user can talk to me. SLACK_USER_IDS is not defined or is empty in config.yaml."
+try:
+    SLACK_CHANNEL_IDS = config['SLACK_CHANNEL_IDS']
+except:
+    SLACK_CHANNEL_IDS = None
+if not SLACK_CHANNEL_IDS:
+    print "I will respond in any channel. SLACK_CHANNEL_IDS is not defined or is empty in config.yaml."
+try:
+    SLACK_USER_ACCESS_DENIED = config['SLACK_USER_ACCESS_DENIED']
+except:
+    SLACK_USER_ACCESS_DENIED = "You do not have permission to use me."
+try:
+    SLACK_CHANNEL_ACCESS_DENIED = config['SLACK_CHANNEL_ACCESS_DENIED']
+except:
+    SLACK_CHANNEL_ACCESS_DENIED = "This channel does not have permission to use me."
+
+try:
+    CEPH_CLUSTER_ID = config['CEPH_CLUSTER_ID']
+except:
+    CEPH_CLUSTER_ID = "ceph"
+try:
+    CEPH_CONF = config['CEPH_CONF']
+except:
+    CEPH_CONF =  "/etc/ceph/ceph.conf"
+try:
+    CEPH_USER = config['CEPH_USER']
+except:
+    CEPH_USER = "client.admin"
+try:
+    CEPH_KEYRING = config['CEPH_KEYRING']
+except:
+    CEPH_KEYRING = "/etc/ceph/ceph.client.admin.keyring"
+
+try:
+    HELP_MSG = CEPH_CLUSTER_ID + ": " + config['HELP_MSG']
+except:
+    HELP_MSG = CEPH_CLUSTER_ID + ": status, osd stat, mon, stat, pg stat"
+try:
     TOO_LONG = config["TOO_LONG"]
+except:
+    TOO_LONG = 20
+try:
     TOO_LONG_MSG = config["TOO_LONG_MSG"]
 except:
-    print "Values missing in the config file. Please compare it to the example."
-    exit()
+    TOO_LONG_MSG = "Response was too long. Check your DMs."
+
 
 HELP = "help"
 AT_BOT = "<@" + SLACK_BOT_ID + ">"
@@ -34,7 +86,7 @@ def ceph_command(command):
     try:
         cluster.connect()
     except:
-        print "Something prevented the connection to the Ceph cluster."
+        print "Something prevented the connection to the Ceph cluster. Check your CEPH_USER and CEPH_KEYRING settings."
         exit()
     cmd = {"prefix":command, "format":"plain"}
     try:
@@ -52,22 +104,33 @@ def ceph_command(command):
 
 
 def handle_command(command, channel, user):
-    command = command.strip().lower()
-    if command.startswith(CEPH_CLUSTER_ID):
-        command = command.split(CEPH_CLUSTER_ID)[1].strip().lower()
-        if command.startswith(HELP):
-            channel_response = HELP_MSG
-            user_response = None
-        else:
-            channel_response, user_response = ceph_command(command)
-    elif command.startswith(HELP):
-        channel_response = HELP_MSG
-        user_response = None
+    if SLACK_USER_IDS and not user in SLACK_USER_IDS:
+        channel_response = None
+        user_response = SLACK_USER_ACCESS_DENIED
     else:
-        return
+        command = command.strip().lower()
+        if command.startswith(CEPH_CLUSTER_ID):
+            command = command.split(CEPH_CLUSTER_ID)[1].strip().lower()
+            if SLACK_CHANNEL_IDS and not channel in SLACK_CHANNEL_IDS:
+                channel_response = SLACK_CHANNEL_ACCESS_DENIED
+                user_response = None
+            elif command.startswith(HELP):
+                channel_response = HELP_MSG
+                user_response = None
+            else:
+                channel_response, user_response = ceph_command(command)
+        elif command.startswith(HELP):
+            if SLACK_CHANNEL_IDS and not channel in SLACK_CHANNEL_IDS:
+                channel_response = None
+                user_response = None
+            else:
+                channel_response = HELP_MSG
+                user_response = None
+        else:
+            return
 
     # Direct Messages have a channel that starts with a 'D'
-    if not ( channel_response and channel.startswith('D') and channel_response == TOO_LONG_MSG ):
+    if channel_response and not ( channel_response and channel.startswith('D') and channel_response == TOO_LONG_MSG ):
         slack_client.api_call("chat.postMessage", channel=channel,
                           text=channel_response, as_user=True)
 
@@ -85,7 +148,7 @@ def parse_slack_output(slack_rtm_output):
                 return output['text'].split(AT_BOT)[1].strip().lower(), \
                        output['channel'], output['user']
             # Direct Messages have a channel that starts with a 'D'
-            if output and 'text' in output and output['channel'].startswith('D') and output['user'] != SLACK_BOT_ID:
+            elif output and 'text' in output and output['channel'].startswith('D') and output['user'] != SLACK_BOT_ID:
                 return output['text'], output['channel'], output['user']
     return None, None, None
 
