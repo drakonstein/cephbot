060126218a3255cc96c6737302c4aa1261330a5e
David Turner <drakonstein@gmail.com>
2020-04-02 09:54:28 -0400

    adding options to control entrypoint output

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-
-if [ -n "$(command -v yum)" ]; then
-  yum -y install python-pip
-elif [ -n "$(command -v apt-get)" ]; then
-  apt-get -y install python-pip
+if [[ "$1" == "quiet" ]]; then
+  quiet="2>&1 > /dev/null"
+else
+  quiet=
 fi
 
-python2 -m pip --disable-pip-version-check install -r /cephbot/requirements.txt
+if [ -n "$(command -v yum)" ]; then
+  yum -y install python-pip $quiet
+elif [ -n "$(command -v apt-get)" ]; then
+  apt-get -y install python-pip $quiet
+fi
+
+python2 -m pip --disable-pip-version-check install -r /cephbot/requirements.txt $quiet
 python2 /cephbot/cephbot.py
