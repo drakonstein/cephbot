a22d2c914693028099751c19e86ad3113c2235cc
David Turner <drakonstein@gmail.com>
2020-04-02 02:12:13 -0400

    re-enabling output for most pod starting commands for ease of troubleshooting in general

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ -n "$(command -v yum)" ]; then
-  yum -y install python-pip 2>&1 > /dev/null
+  yum -y install python-pip
 elif [ -n "$(command -v apt-get)" ]; then
-  apt-get -y install python-pip 2>&1 > /dev/null
+  apt-get -y install python-pip
 fi
 
-python2 -m pip --disable-pip-version-check install -r /cephbot/requirements.txt 2>&1 > /dev/null
+python2 -m pip --disable-pip-version-check install -r /cephbot/requirements.txt
 python2 /cephbot/cephbot.py
