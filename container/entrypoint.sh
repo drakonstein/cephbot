20abde3a2f014104234116cf799ff430835df72a
David Turner <drakonstein@gmail.com>
2020-04-02 09:59:18 -0400

    fixing syntax for output suppression

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 if [[ "$1" == "quiet" ]]; then
-  quiet="2>&1 > /dev/null"
+  quiet=true
 else
-  quiet=
+  quiet=false
 fi
 
 if [ -n "$(command -v yum)" ]; then
-  yum -y install python-pip $quiet
+  cmd="yum -y install python-pip"
 elif [ -n "$(command -v apt-get)" ]; then
-  apt-get -y install python-pip $quiet
+  cmd="apt-get -y install python-pip"
+fi
+cmd2="python2 -m pip --disable-pip-version-check install -r /cephbot/requirements.txt"
+if $quiet; then
+  $cmd > /dev/null 2>&1
+  $cmd2 > /dev/null 2>&1
+else
+  $cmd
+  $cmd2
 fi
 
-python2 -m pip --disable-pip-version-check install -r /cephbot/requirements.txt $quiet
 python2 /cephbot/cephbot.py
