8900d9afdb66070d9914b43a16cc209f3ce2208f
David Turner <dturner@rsglab.com>
2021-05-18 12:08:35 -0400

    updating spacing

--- find_bot_id.py
+++ find_bot_id.py
@@ -9,12 +9,12 @@ SLACK_BOT_NAME = os.getenv('SLACK_BOT_NAME')
 slack_client = SlackClient(SLACK_BOT_TOKEN)
 
 if __name__ == "__main__":
-    api_call = slack_client.api_call("users.list")
-    if api_call.get('ok'):
-        # retrieve all users so we can find our bot
-        users = api_call.get('members')
-        for user in users:
-            if 'name' in user and user.get('name') == SLACK_BOT_NAME:
-                print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
-    else:
-        print("could not find bot user with the name " + SLACK_BOT_NAME)
+  api_call = slack_client.api_call("users.list")
+  if api_call.get('ok'):
+    # retrieve all users so we can find our bot
+    users = api_call.get('members')
+    for user in users:
+      if 'name' in user and user.get('name') == SLACK_BOT_NAME:
+        print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
+  else:
+    print("could not find bot user with the name " + SLACK_BOT_NAME)
