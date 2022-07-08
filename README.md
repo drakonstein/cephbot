68798ad29cbedda19e261cea0468ac39c3b4d498
David Turner <david.turner1@sony.com>
2022-07-08 14:34:32 -0400

    Updating cephbot to be able to handle multiple clusters in 1 container

--- README.md
+++ README.md
@@ -3,13 +3,11 @@ Slack bot for Ceph
 
 There are instructions for how to deploy this to kubernetes in the kubernetes folder of this repo.
 
-I got my start for interfacing with slack from here. Thank you Matt Makai. https://www.fullstackpython.com/blog/build-first-slack-bot-python.html
+The setup for this is pretty simple. You need to find the Slack Bot's Token, Name, and ID. There is an example script to get the Bot's ID. The Token and Name will be visible in the Bot name on Slack's website for it.
 
-The setup for this is pretty simple.  You need to find the Slack Bot's Token, Name, and ID.  There is an example script to get the Bot's ID.  The Token and Name will be visible in the Bot name on Slack's website for it.
+I generally create a client.cephbot user in Ceph for this `ceph auth get-or-create client.cephbot mon 'allow r' mgr 'allow r' > /etc/ceph/ceph.client.cephbot.keyring` which prevents people in Slack for figuring out ways of performing operations against the cluster. If you do enable cephbot to perform maintenance on the cluster, I highly recommend that you utilize the SLACK_USER_IDS and SLACK_CHANNEL_IDS config options to limit who can interface with cephbot.
 
-I generally create a client.cephbot user in Ceph for this `ceph auth get-or-create client.cephbot mon 'allow r' mgr 'allow r' > /etc/ceph/ceph.client.cephbot.keyring` which prevents people in Slack for figuring out ways of performing operations against the cluster.  If you do enable cephbot to perform maintenance on the cluster, I highly recommend that you utilize the SLACK_USER_IDS and SLACK_CHANNEL_IDS config options to limit who can interface with cephbot.
-
-Required non-standard Python libraries are slackclient which can be installed via pip.
+Required non-standard Python libraries are slack_sdk which can be installed via pip.
 
 ``` bash
 pip install -r requirements.txt
@@ -17,10 +15,10 @@ pip install -r requirements.txt
 
 The server this is running on needs to have ceph installed (for bash scripts in the scripts/ directory and the rados python library) and needs to be able to communicate with the mons, but otherwise does not require any other cluster integrations.
 
-To run this for multiple clusters, spin up multiple instances of it with a different CEPH_CLUSTER_ID in the config as well as a different CEPH_CONF and CEPH_KEYRING for each additional cluster.  I like to do this in a screen with multiple windows on the screen from a server that can communicate with all of the ceph clusters (a master admin node), but running each on a different server for each cluster would work as well as long as the server can also communicate with Slack.
+Cephbot is now capable of communicating with multiple clusters in one instance. CEPH_CLUSTER_IDS is in the format of "cluster1: alias1 alias2 alias3, cluster2: alias1 alias2, cluster3: ...". For CEPH_CONF and CEPH_KEYRING you can use CLUSTER as a literal string in the variable that will be replaced by the cluster name specified in CEPH_CLUSTER_IDS. If you are not running this in kubernetes, I would run it in a screen with the environment variables set up. Each instance can be configured for 1 or all of your clusters.
 
 ## Execution
-Update the .env files with your values.  Then execute the following:
+Update the .env files with your values. Then execute the following:
 
 ``` bash
 source .env
--- cephbot.py
+++ cephbot.py
@@ -28,8 +28,7 @@ if not SLACK_CHANNEL_IDS:
 SLACK_USER_ACCESS_DENIED = os.getenv('SLACK_USER_ACCESS_DENIED', "You do not have permission to use me.")
 SLACK_CHANNEL_ACCESS_DENIED = os.getenv('SLACK_CHANNEL_ACCESS_DENIED', "This channel does not have permission to use me.")
 
-CEPH_CLUSTER_ID = os.getenv('CEPH_CLUSTER_ID', "ceph").strip().lower()
-CLUSTER_ALIASES = [i.lower() for i in os.getenv('CLUSTER_ALIASES', "all").strip().split()]
+CEPH_CLUSTER_IDS = os.getenv('CEPH_CLUSTER_IDS', "ceph: all").strip().lower()
 
 SCRIPTS_FOLDER = os.getenv('SCRIPTS_FOLDER', './scripts')
 
@@ -37,29 +36,49 @@ CEPH_CONF = os.getenv('CEPH_CONF', "/etc/ceph/ceph.conf")
 CEPH_USER = os.getenv('CEPH_USER', "client.admin")
 CEPH_KEYRING = os.getenv('CEPH_KEYRING', "/etc/ceph/ceph.client.admin.keyring")
 
-HELP_MSG = CEPH_CLUSTER_ID + ": " + os.getenv('HELP_MSG', "status, osd stat, mon, stat, pg stat, down osds, blocked requests").strip()
+HELP_MSG = ": " + os.getenv('HELP_MSG', "status, osd stat, mon, stat, pg stat, down osds, blocked requests").strip()
 TOO_LONG = os.getenv("TOO_LONG", 20)
 TOO_LONG_MSG = "Long responses get threaded."
+ALWAYS_THREAD = os.getenv('ALWAYS_THREAD', 'false').lower() in ('true', '1', 't')
+ALWAYS_SHOW_CLUSTER_ID = os.getenv('ALWAYS_SHOW_CLUSTER_ID', 'false').lower() in ('true', '1', 't')
 
 HELP = "help"
 AT_BOT = "<@" + SLACK_BOT_ID + ">"
+CEPH_CLUSTERS = {}
+for cluster in CEPH_CLUSTER_IDS.split(","):
+  cluster = cluster.split(":")
+  CEPH_CLUSTERS[cluster[0].strip()] = cluster[1].strip()
 
 rtm = RTMClient(token=SLACK_BOT_TOKEN)
 
 
-def ceph_command(command, thread):
-  cluster = rados.Rados(conffile=CEPH_CONF, conf=dict(keyring = CEPH_KEYRING), name=CEPH_USER)
+def ceph_command(CLUSTER, command, thread):
+  ceph_conf = CEPH_CONF
+  if "CLUSTER" in ceph_conf:
+    ceph_conf = ceph_conf.replace("CLUSTER", CLUSTER)
+
+  ceph_keyring = CEPH_KEYRING
+  if "CLUSTER" in ceph_keyring:
+    ceph_keyring = ceph_keyring.replace("CLUSTER", CLUSTER)
+  if "CEPH_USER" in ceph_keyring:
+    ceph_keyring = ceph_keyring.replace("CEPH_USER", CEPH_USER)
+
+
+  cluster = rados.Rados(conffile=ceph_conf, conf=dict(keyring = ceph_keyring), name=CEPH_USER)
   run_mon_command = True
   try:
     cluster.connect()
   except:
-    print("Something prevented the connection to the Ceph cluster. Check your CEPH_USER and CEPH_KEYRING settings.")
-    exit()
+    print("Something prevented the connection to the Ceph cluster. Attempted using the following settings.")
+    print("CEPH_CONF: " + ceph_conf)
+    print("CEPH_USER: " + CEPH_USER)
+    print("CEPH_KEYRING: " + ceph_keyring)
+    return "Something went wrong while connecting to " + CLUSTER + " using " + ceph_conf + ", " + CEPH_USER + ", " + ceph_keyring, None
 
   if command == "blocked requests":
     run_mon_command = False
     try:
-      output = subprocess.check_output(['/usr/bin/timeout', '5', SCRIPTS_FOLDER + '/blocked_requests.sh', '--conf', CEPH_CONF, '--user', CEPH_USER, '--keyring', CEPH_KEYRING])
+      output = subprocess.check_output(['/usr/bin/timeout', '5', SCRIPTS_FOLDER + '/blocked_requests.sh', '--conf', ceph_conf, '--user', CEPH_USER, '--keyring', ceph_keyring])
     except:
       return "Something went wrong while executing " + command + " on the Ceph cluster.", None
   elif command == "down osds" or command == "down osd":
@@ -67,7 +86,7 @@ def ceph_command(command, thread):
   elif command == "io":
     run_mon_command = False
     try:
-      output = subprocess.check_output(['/usr/bin/timeout', '5', SCRIPTS_FOLDER + '/io.sh', '--conf', CEPH_CONF, '--user', CEPH_USER, '--keyring', CEPH_KEYRING])
+      output = subprocess.check_output(['/usr/bin/timeout', '5', SCRIPTS_FOLDER + '/io.sh', '--conf', ceph_conf, '--user', CEPH_USER, '--keyring', ceph_keyring])
     except:
       return "Something went wrong while executing " + command + " on the Ceph cluster.", None
   elif command.startswith("pool io"):
@@ -75,18 +94,18 @@ def ceph_command(command, thread):
     run_mon_command = False
     if opt_pool == None:
       try:
-        output = subprocess.check_output(['/usr/bin/timeout', '5', SCRIPTS_FOLDER + '/pool_io.sh', '--conf', CEPH_CONF, '--user', CEPH_USER, '--keyring', CEPH_KEYRING])
+        output = subprocess.check_output(['/usr/bin/timeout', '5', SCRIPTS_FOLDER + '/pool_io.sh', '--conf', ceph_conf, '--user', CEPH_USER, '--keyring', ceph_keyring])
       except:
         return "Something went wrong while executing " + command + " on the Ceph cluster.", None
     else:
       try:
-        output = subprocess.check_output(['/usr/bin/timeout', '5', SCRIPTS_FOLDER + '/pool_io.sh', '--conf', CEPH_CONF, '--user', CEPH_USER, '--keyring', CEPH_KEYRING, '--pool', opt_pool])
+        output = subprocess.check_output(['/usr/bin/timeout', '5', SCRIPTS_FOLDER + '/pool_io.sh', '--conf', ceph_conf, '--user', CEPH_USER, '--keyring', ceph_keyring, '--pool', opt_pool])
       except:
         return "Something went wrong while executing " + command + " on the Ceph cluster.", None
   elif command.startswith("health detail"):
     run_mon_command = False
     try:
-      output = subprocess.check_output(['/usr/bin/timeout', '5', SCRIPTS_FOLDER + '/health_detail.sh', '--conf', CEPH_CONF, '--user', CEPH_USER, '--keyring', CEPH_KEYRING])
+      output = subprocess.check_output(['/usr/bin/timeout', '5', SCRIPTS_FOLDER + '/health_detail.sh', '--conf', ceph_conf, '--user', CEPH_USER, '--keyring', ceph_keyring])
     except:
       return "Something went wrong while executing " + command + " on the Ceph cluster.", None
   else:
@@ -129,7 +148,7 @@ def ceph_command(command, thread):
   elif output and len(output.splitlines()) >= int(TOO_LONG):
     return TOO_LONG_MSG, output
   else:
-    return "Something went wrong while executing '" + command + "' on the Ceph cluster.", None
+    return "Something went wrong while executing '" + command + "' on " + CLUSTER + ".", None
 
 
 @rtm.on("message")
@@ -139,10 +158,6 @@ def parse_slack(client: RTMClient, event: dict):
     command = event['text']
     channel = event['channel']
     user = event['user']
-    if 'thread_ts' in event:
-      thread = event['thread_ts']
-    else:
-      thread = None
     if AT_BOT in command:
       for_cephbot = True
       command = command.split(AT_BOT, 1)[1]
@@ -150,18 +165,39 @@ def parse_slack(client: RTMClient, event: dict):
       for_cephbot = True
 
   if for_cephbot:
-    show_cluster_id = False
+    if 'thread_ts' in event:
+      thread = event['thread_ts']
+    else:
+      if ALWAYS_THREAD:
+        thread = event['ts']
+      else:
+        thread = None
+    show_cluster_id = ALWAYS_SHOW_CLUSTER_ID
+
     command = command.strip().lower()
     cluster = command.split()[0]
     cluster_match = False
-    if cluster == CEPH_CLUSTER_ID:
-      cluster_match = True
-    elif cluster in CLUSTER_ALIASES:
-      cluster_match = True
-      show_cluster_id = True
+    clusters_matched = []
+    if command.startswith(HELP):
+      clusters_matched = CEPH_CLUSTERS
+    for CLUSTER in CEPH_CLUSTERS:
+      if cluster == CLUSTER:
+        cluster_match = True
+        clusters_matched.append(CLUSTER)
+      else:
+        for ALIAS in CEPH_CLUSTERS[CLUSTER].split():
+          if cluster == ALIAS.strip():
+            cluster_match = True
+            clusters_matched.append(CLUSTER)
+            show_cluster_id = True
+            break
 
     if cluster_match:
       command = command.split(cluster, 1)[1].strip().lower()
+    channel_response = None
+    user_response = None
+
+    for CLUSTER in clusters_matched:
       if SLACK_USER_IDS and not user in SLACK_USER_IDS:
         channel_response = None
         user_response = SLACK_USER_ACCESS_DENIED
@@ -169,59 +205,51 @@ def parse_slack(client: RTMClient, event: dict):
         channel_response = SLACK_CHANNEL_ACCESS_DENIED
         user_response = None
       elif command.startswith(HELP):
-        channel_response = HELP_MSG
+        help_msg = HELP_MSG
+        if "ALIASES" in help_msg:
+          help_msg = help_msg.replace("ALIASES", CEPH_CLUSTERS[CLUSTER])
+        channel_response = CLUSTER + help_msg
         user_response = None
       else:
-        channel_response, user_response = ceph_command(command, thread)
-    elif command.startswith(HELP):
-      if SLACK_CHANNEL_IDS and not channel.startswith('D') and not channel in SLACK_CHANNEL_IDS:
-        channel_response = None
-        user_response = None
-      elif SLACK_USER_IDS and not user in SLACK_USER_IDS:
-        channel_response = None
-        user_response = None
-      else:
-        channel_response = HELP_MSG
-        user_response = None
-    else:
-      channel_response = None
-      user_response = None
-    response = None
+        channel_response, user_response = ceph_command(CLUSTER, command, thread)
+      response = None
 
-    # Direct Messages have a channel that starts with a 'D'
-    if channel_response and not ( channel_response and channel.startswith('D') and channel_response == TOO_LONG_MSG ):
-      if show_cluster_id:
-        channel_response = CEPH_CLUSTER_ID + ": " + channel_response
-      if thread:
-        client.web_client.chat_postMessage(
-          channel=channel, 
-          thread_ts=thread, 
-          text=channel_response, 
-          as_user=True
-        )
-      else:
-        response = client.web_client.chat_postMessage(channel=channel, text=channel_response, as_user=True)
+      # Direct Messages have a channel that starts with a 'D'
+      if channel_response and not ( channel_response and channel.startswith('D') and channel_response == TOO_LONG_MSG ):
+        if show_cluster_id:
+          if not channel_response.startswith(CLUSTER):
+            channel_response = CLUSTER + ": " + channel_response
+        if thread:
+          client.web_client.chat_postMessage(
+            channel=channel,
+            thread_ts=thread,
+            text=channel_response,
+            as_user=True
+          )
+        else:
+          response = client.web_client.chat_postMessage(channel=channel, text=channel_response, as_user=True)
 
-    if user_response:
-      if channel_response and channel_response == TOO_LONG_MSG and response:
-        thread = response['ts']
-      else:
-        thread = None
-      if show_cluster_id:
-        user_response = CEPH_CLUSTER_ID + ": " + user_response
-      if thread:
-        client.web_client.chat_postMessage(
-          channel=channel, 
-          thread_ts=thread, 
-          text=user_response, 
-          as_user=True
-        )
-      else:
-        client.web_client.chat_postMessage(
-          channel=user, 
-          text=user_response, 
-          as_user=True
-        )
+      if user_response:
+        if channel_response and channel_response == TOO_LONG_MSG and response:
+          thread = response['ts']
+        else:
+          thread = None
+        if show_cluster_id:
+          if not channel_response.startswith(CLUSTER):
+            user_response = CLUSTER + ": " + user_response
+        if thread:
+          client.web_client.chat_postMessage(
+            channel=channel,
+            thread_ts=thread,
+            text=user_response,
+            as_user=True
+          )
+        else:
+          client.web_client.chat_postMessage(
+            channel=user,
+            text=user_response,
+            as_user=True
+          )
 
 try:
   rtm.start()
